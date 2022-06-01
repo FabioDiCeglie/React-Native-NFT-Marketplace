b88b2e74741eb3f843d7c7c7dd649fa7607331b7
FabioDiCeglie <dcfabio96@gmail.com>
2022-06-01 22:35:50 +0200

    add package.json

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My firs react app</Text>
-      <StatusBar style="auto" />
+      <Text>Hello</Text>
     </View>
   );
 }
