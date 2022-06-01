abffbce100c7283561d1dd037ef60e78044f3cc5
FabioDiCeglie <dcfabio96@gmail.com>
2022-06-01 23:16:07 +0200

    create status bar

---
+++ components/HomeHeader.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { View, SafeAreaView, FlatList, Text } from "react-native";
+
+import { COLORS, NFTData } from "../constants";
+
+const HomeHeader = () => {
+  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>;
+};
+
+export default HomeHeader;
---
+++ components/NFTCard.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { View, SafeAreaView, FlatList, Text } from "react-native";
+
+import { COLORS, NFTData } from "../constants";
+
+const NFTCard = () => {
+  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>;
+};
+
+export default NFTCard;
