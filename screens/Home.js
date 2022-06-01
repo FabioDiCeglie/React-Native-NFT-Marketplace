00fd0c5866051083dd9f46617808ef43fd90bdda
FabioDiCeglie <dcfabio96@gmail.com>
2022-06-01 23:04:05 +0200

    add dummy data

--- screens/Home.js
+++ screens/Home.js
@@ -2,8 +2,9 @@ import { useState } from "react";
 import { View, SafeAreaView, FlatList, Text } from "react-native";
 
 import { COLORS, NFTData } from "../constants";
+
 const Home = () => {
-  return <Text>Home</Text>;
+  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>;
 };
 
 export default Home;
