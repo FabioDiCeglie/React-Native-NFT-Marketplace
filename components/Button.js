49b8b6eb3ddbab95e90e409605e651c14fa575b5
FabioDiCeglie <dcfabio96@gmail.com>
2022-06-15 21:17:56 +0200

    create rect button

--- components/Button.js
+++ components/Button.js
@@ -26,10 +26,30 @@ export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
   );
 };
 
-export const RectButton = ({ data }) => {
+export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
   return (
     <View>
-      <Text></Text>
+      <TouchableOpacity
+        style={{
+          backgroundColor: COLORS.primary,
+          borderRadius: SIZES.extraLarge,
+          minWidth: minWidth,
+          padding: SIZES.small,
+          ...props,
+        }}
+        onPress={handlePress}
+      >
+        <Text
+          style={{
+            fontFamily: FONTS.semiBold,
+            fontSize: fontSize,
+            color: COLORS.white,
+            textAlign: "center",
+          }}
+        >
+          Place a bid
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
